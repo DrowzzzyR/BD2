8daa0b201edf86d45f05cff37dc454c0fdf68cd7
goldeneagle101 <school17mt@mail.ru>
2024-01-12 17:15:17 +0300

    fix error, etc.

--- myflaskapp/app.py
+++ myflaskapp/app.py
@@ -3,14 +3,46 @@ from flask_mysqldb import MySQL
 from werkzeug.security import generate_password_hash, check_password_hash
 from functools import wraps
 from config import DB_CONFIG, SECRET_KEY
+from config import DB_CONFIG
+from sqlalchemy import MetaData
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
+
 app.config['MYSQL_HOST'] = DB_CONFIG['host']
 app.config['MYSQL_USER'] = DB_CONFIG['user']
 app.config['MYSQL_PASSWORD'] = DB_CONFIG['password']
 app.config['MYSQL_DB'] = DB_CONFIG['database']
 app.config['SECRET_KEY'] = SECRET_KEY
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] =  f'mysql+mysqlconnector://{DB_CONFIG.get("user")}:{DB_CONFIG.get("password")}@std-mysql.ist.mospolytech.ru/{DB_CONFIG.get("database")}'
+
 mysql = MySQL(app)
+
+convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+
+metadata = MetaData(naming_convention=convention)
+db = SQLAlchemy(app, metadata=metadata)
+migrate = Migrate(app, db)
+
+from models import *
+
+# from auth import bp as auth_bp
+# app.register_blueprint(auth_bp)
+
+# Маршрут для корневой страницы
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 
 # Функция для проверки авторизации пользователя
 def login_required(role='operator'):
@@ -31,58 +63,6 @@ def login_required(role='operator'):
             return redirect(url_for('login'))
         return decorated_view
     return wrapper
-
-# Регистрация пользователя
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        role = request.form['role']
-        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')  # Используем pbkdf2_sha256 для хэширования
-
-        cursor = mysql.connection.cursor()
-        cursor.execute("INSERT INTO users (username, password_hash, role) VALUES (%s, %s, %s)",
-                       (username, hashed_password, role))
-        mysql.connection.commit()
-        cursor.close()
-
-        flash('Регистрация прошла успешно', 'success')
-        return redirect(url_for('login'))
-
-    return render_template('register.html')
-
-# Авторизация пользователя
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-
-        cursor = mysql.connection.cursor()
-        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
-        user = cursor.fetchone()
-        cursor.close()
-
-        if user and check_password_hash(user[2], password):  
-            session['user_id'] = user[0]
-            flash('Добро пожаловать, {}'.format(username), 'success')
-            return redirect(url_for('dashboard'))
-        else:
-            flash('Неверные учетные данные', 'danger')
-
-    return render_template('login.html')
-
-# Выход из системы
-@app.route('/logout')
-def logout():
-    session.pop('user_id', None)
-    return redirect(url_for('login'))
-
-# Маршрут для корневой страницы
-@app.route('/')
-def index():
-    return render_template('index.html')
 
 @app.route('/dashboard')
 @login_required()  # Предполагается, что это эндпоинт, доступный только авторизованным пользователям
@@ -143,25 +123,78 @@ def add_supply():
 @app.route('/products')
 def products():
     # Подключение к базе данных
-    cursor = mysql.connection.cursor()
+    # cursor = mysql.connection.cursor()
 
-    # Выполнение запроса к базе данных для объединения данных из таблиц products и supplies
-    cursor.execute("""
-        SELECT p.id, p.name, p.description, p.price, s.id, s.product_id, s.quantity, s.supply_date
-        FROM products p
-        LEFT JOIN supplies s ON p.id = s.product_id
-    """)
+    # # Выполнение запроса к базе данных для объединения данных из таблиц products и supplies
+    # cursor.execute("""
+    #     SELECT p.id, p.name, p.description, p.price, s.id, s.product_id, s.quantity, s.supply_date
+    #     FROM products p
+    #     LEFT JOIN supplies s ON p.id = s.product_id
+    # """)
+    
+    # # Получение результатов запроса
+    # products_data = cursor.fetchall()
 
-    # Получение результатов запроса
-    products_data = cursor.fetchall()
+    # # Закрытие соединения с базой данных
+    # cursor.close()
 
-    # Закрытие соединения с базой данных
-    cursor.close()
-
-    print(products_data)
+    products_data = Product.query.all()
+    supplies_data = Supply.query.all()
 
     # Передача данных в шаблон и отображение страницы
-    return render_template('products.html', products=products_data)
+    return render_template('products.html', products=products_data, supplies=supplies_data)
+
+
+
+
+
+# Регистрация пользователя
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        role = request.form['role']
+        hashed_password = generate_password_hash(password, method='pbkdf2:sha256')  # Используем pbkdf2_sha256 для хэширования
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("INSERT INTO users (username, password_hash, role) VALUES (%s, %s, %s)",
+                       (username, hashed_password, role))
+        mysql.connection.commit()
+        cursor.close()
+
+        flash('Регистрация прошла успешно', 'success')
+        return redirect(url_for('login'))
+
+    return render_template('register.html')
+
+# Авторизация пользователя
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        cursor = mysql.connection.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = %s", (username,))
+        user = cursor.fetchone()
+        cursor.close()
+
+        if user and check_password_hash(user[2], password):  
+            session['user_id'] = user[0]
+            flash('Добро пожаловать, {}'.format(username), 'success')
+            return redirect(url_for('dashboard'))
+        else:
+            flash('Неверные учетные данные', 'danger')
+
+    return render_template('login.html')
+
+# Выход из системы
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('login'))
+
 
 
 if __name__ == '__main__':
---
+++ myflaskapp/models.py
@@ -0,0 +1,42 @@
+import sqlalchemy as sa
+from werkzeug.security import check_password_hash, generate_password_hash
+from flask_login import UserMixin
+from flask import current_app
+from app import db
+
+
+class Product(db.Model):
+    __tablename__ = "products"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+
+    description = db.Column(db.String(100))
+    
+    price = db.Column(db.Integer, nullable=False)
+
+
+
+class Supply(db.Model):
+    __tablename__ = "supplies"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    product_id = db.Column(db.Integer, db.ForeignKey("products.id"), default=None)
+
+    quantity = db.Column(db.Integer,  nullable=False)
+    
+    price = db.Column(db.Date, nullable=False)
+
+    product = db.relationship('Product')
+
+
+class User(db.Model, UserMixin):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(100), nullable=False)
+    
